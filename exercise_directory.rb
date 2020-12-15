f038af4b59b46b0aa083ff86312f21f81589fce4
Glykeria Stravodimou <strglykeria@gmail.com>
2020-12-15 14:29:45 +0000

    Center method

--- exercise_directory.rb
+++ exercise_directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(50)
+  puts "To finish, just hit return twice".center(50)
 
 #create an empty array
   students = []
@@ -11,8 +11,8 @@ def input_students
 # while the name is not empty repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :November}
-    puts "Now we have #{students.count} students"
+    students << {name: name}
+    puts "Now we have #{students.count} students".center(50)
     # get another name from the user
     name = gets.chomp
   end
@@ -23,34 +23,33 @@ def input_students
 end
 
 def input_country(students)
-  puts "Please enter the country of birth of the student"
-  puts "To finish, just hit return twice"
+  puts "Please enter the country of birth of the student".center(50)
+  puts "To finish, just hit return twice".center(50)
   country = gets.chomp
   while !country.empty? do
     n = 0
     while n < students.count do
       students[n][:country] = country
-      puts "#{students[n][:name]}'s country of birth is #{country}"
+      puts "#{students[n][:name]}'s country of birth is #{country}".center(50)
       n += 1
       country = gets.chomp
     end
   end
-  p students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 def print(students)
   students.each do |name|
-    puts "#{name[:name]}, #{name[:cohort]} cohort, #{name[:country]}"
+    puts "#{name[:name]}, #{name[:cohort]} cohort, #{name[:country]}".center(20)
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(20)
 end
 
 # nothing happens until we call the methods
