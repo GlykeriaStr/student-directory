2b86dc49c856326778910ba47a6fa9696fc4f4bf
Glykeria Stravodimou <strglykeria@gmail.com>
2020-12-18 15:20:06 +0000

    Saving students on files

--- directory.rb
+++ directory.rb
@@ -33,14 +33,33 @@ def process(selection)
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  name = STDIN.gets.chomp
-  while !name.empty? do
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    name = STDIN.gets.chomp
+  months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
+  puts "Please enter the names of the student and the cohort they are in separated by a comma".center(50)
+  puts "To finish, just hit return twice".center(50)
+
+  name_cohort = STDIN.gets.delete("\n")
+
+  while !name_cohort.empty? do
+    name, cohort = name_cohort.split(", ")
+    if cohort != nil && months.include?(cohort.to_sym.capitalize)
+      @students << {name: name, cohort: cohort.to_sym.capitalize}
+    elsif cohort != nil && !months.include?(cohort.to_sym.capitalize)
+      puts "Please enter a valid cohort".center(50)
+    elsif cohort == nil
+      @students << {name: name, cohort: :November}
+    end
+
+    if @students.count == 1
+      puts "Now we have #{@students.count} student".center(50)
+    else
+      puts "Now we have #{@students.count} students".center(50)
+    end
+    name_cohort = STDIN.gets.delete("\n")
   end
+  if @students.empty?
+    exit
+  end
+  @students
 end
 
 def show_students
@@ -68,14 +87,22 @@ def save_students
   puts "What would you like to name the file?"
   filename = STDIN.gets.chomp
   filename += ".csv" if filename.split(".").length == 1
+  filename = "students.csv" if filename.split(".").length == 0
 
-  if filename.split(".")[1] == "csv"
-    file = File.open(filename, "w")
+  if filename.split(".")[0] == nil
+    file = File.open("students.csv", "w")
     @students.each do |student|
       student_data = [student[:name], student[:cohort]]
       csv_line = student_data.join(",")
       file.puts csv_line
     end
+  elsif filename.split(".")[1] == "csv"
+  file = File.open(filename, "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
   else
     puts "The file will be saved as 'csv'.\nPlease enter a filename with or without the '.csv' extension."
   end
@@ -84,6 +111,7 @@ def save_students
 end
 
 def load_students(filename = "students.csv")
+
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
